d6acd596019789ab5b1036a16a47723007dd284c
Neoon <neo@tiefkuehler-madness.me>
2023-06-03 14:19:31 +0200

    update

---
+++ seed.py
@@ -0,0 +1,41 @@
+import subprocess, netaddr, pyasn
+
+def networkToSubs(subnet):
+    sub, prefix = subnet.split("/")
+    if int(prefix) > 22: return [subnet]
+    network = netaddr.IPNetwork(subnet)
+    return [str(sn) for sn in network.subnet(23)]
+
+print("Microsoft: 8075, AWS: 16509")
+asns = input("Please input asn numbers, eg. 3000,4000: ")
+asns = asns.split(",")
+
+print(asns)
+
+print("Building list")
+
+ips = []
+with open('asn.dat') as file:
+    for line in file:
+        if ";" in line: continue
+        line = line.rstrip()
+        subnet, asn = line.split("\t")
+        if asn in asns:
+            subs = networkToSubs(subnet)
+            for sub in subs:
+                ip, prefix = sub.split("/")
+                ip = ip[:-1]
+                ip = f"{ip}1"
+                ips.append(ip)
+
+print("Seeding...")
+batchSize,count,pings = 1000,0,1
+while count <= len(ips):
+    print(f"fping {count} of {len(ips)}")
+    batch = ' '.join(ips[count:count+batchSize])
+    if not batch: break
+    p = subprocess.run(f"fping -c {pings} {batch}", stdin=None, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+    if not p.stdout.decode('utf-8'):
+        print("Please install fping (apt-get install fping / yum install fping)")
+        exit()
+    count += batchSize
